de8df9ad78ef9dcd19aa027493ca5d64a15bc430
dridinouhay <nouhaila.dridi@esprit.tn>
2022-03-01 15:49:21 +0100

    ajout commentaire

---
+++ src/main/java/tn/esprit/RepositoryLocation.java
@@ -0,0 +1,14 @@
+package tn.esprit;
+import java.io.File;
+public class RepositoryLocation {
+
+public static void main(String[] args) {
+File file = new File("C:\\Users\\nameUser\\.m2\\repository\\tn\\esprit\\1.0");
+String absolutePath = file.getAbsolutePath();
+String filePath =absolutePath.substring(0,absolutePath.indexOf(File.separator,
+30));
+System.out.println("Le dossier contenant les dépendances "
++ "et les livrables des projets est : ");
+System.out.println( filePath);
+}
+}
